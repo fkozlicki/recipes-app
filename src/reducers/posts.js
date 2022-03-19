82b434a53feca578619a6e0728252b18a1bb7e05
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-03-19 11:58:53 +0100

    reducer export

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,4 @@
-export default (posts = [], action) => {
+const posts = (posts = [], action) => {
 	switch (action.type) {
 		case "FETCH_ALL":
 			return action.payload;
@@ -14,3 +14,4 @@ export default (posts = [], action) => {
 			return posts;
 	}
 };
+export default posts;
