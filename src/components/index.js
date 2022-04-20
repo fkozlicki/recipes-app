9d314c1b4e0d772d9dd790dd85581b3a134bb7a3
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-20 17:40:05 +0200

    exports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Form } from "./Form/Form";
+export { default as Header } from "./Header/Header";
+export { default as Loader } from "./Loader/Loader";
+export { default as Posts } from "./Posts/Posts";
